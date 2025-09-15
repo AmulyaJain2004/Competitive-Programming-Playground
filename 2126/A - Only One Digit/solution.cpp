1848b77e7807ad387a49f1fb4721bdff6302dc72
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2025-09-15 10:28:25 +0530

    Add Only One Digit [A] from Codeforces

---
+++ 2126/A - Only One Digit/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std;int main(){int t;cin>>t; while(t--){     int x;     cin>>x;    // vector<int> sll_num;    int sn=INT_MAX;    while(x!=0){        int am=x%10;        x=x/10;        sn=min(sn,am);    }    cout<<sn<<endl; }}
