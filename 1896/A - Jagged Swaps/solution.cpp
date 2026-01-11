52586cb3f79863dd30b42ec0713e45753bf9681a
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-01-11 17:43:44 +0530

    Add Jagged Swaps [A] from Codeforces

---
+++ 1896/A - Jagged Swaps/solution.cpp
@@ -0,0 +1,1 @@
+#include <iostream>#include <vector>using namespace std; int main(){    int t;    cin >> t;    while (t--) {        int n;        cin >> n;        vector<int> arr(n);        for (int i = 0; i < n; i++) {            cin >> arr[i];        }        if (arr[0] == 1) {            cout << "Yes\n";        }        else {            cout << "No\n";        }    }    return 0;}
