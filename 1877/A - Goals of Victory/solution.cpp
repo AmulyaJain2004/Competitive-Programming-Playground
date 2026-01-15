a64043e90c254a55b3d67008aacead5975e3bdbe
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-01-15 18:09:05 +0530

    Add Goals of Victory [A] from Codeforces

---
+++ 1877/A - Goals of Victory/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {    int t;    cin >> t;    while (t--) {        int n;        cin >> n;        vector<int> a(n-1);        int summ = 0;        for (int i = 0; i < n-1; i++) {            cin >> a[i];            summ += a[i];        }        cout << -1 * summ << "\n";    }    return 0;}
