93d2399a96d3e8528e36ac889f58e918245cc23d
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2026-05-24 12:50:44 +0530

    Add Watermelon [A] from Codeforces

--- 4/A - Watermelon/solution.cpp
+++ 4/A - Watermelon/solution.cpp
@@ -1,1 +1,16 @@
-#include <bits/stdc++.h>using namespace std; int main () {    int w;    cin >> w;    if (w!=2 && w%2==0) {        cout << "YES\n";    }    else {        cout << "NO\n";    }    return 0;}
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main () {
+    int w;
+    cin >> w;
+    if (w != 2 && w % 2 == 0) {
+        cout << "YES
+";
+    }
+    else {
+        cout << "NO
+";
+    }
+    return 0;
+}
